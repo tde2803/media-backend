5da86c15cd000ba83b2cf943d9961a62176cffcb
Dmitry Tychinin <tde2803@gmail.com>
2024-11-04 15:29:32 +0300

    Initialized spring boot project with maven

---
+++ src/test/java/by/tde2803/media/MediaApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tde2803.media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
